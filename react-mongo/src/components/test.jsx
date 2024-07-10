40bc4d643a7c0a6c8fc7f56fa41e87a698eaad3a
Ile16 <ile.coc95@outlook.com>
2024-07-10 22:40:55 +0200

    edit

--- react-mongo/src/components/test.jsx
+++ react-mongo/src/components/test.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 function Test(){
+const [currentPage, setCurrentPage] = useState(1);
+const [totalPages, setTotalPages] = useState(1);
+const [limit, setLimit] = useState(3);
 const [user, setUser] = useState([]);
 const [newUser, setNewUser] = useState({name:'', email:'', role:'user' });
 
     useEffect(() => {
-        fetch("http://localhost:5001/api/users")
+        fetch(`http://localhost:5001/api/users?page=${currentPage}&limit=${limit}`)
         .then(response => response.json())
-        .then(data => setUser(data.users)) //gli utenti sono all'interno di una chiave nel db che sarebbe users
+        .then(data => {
+            setUser(data.users),
+            setTotalPages(data.totalPages)
+        }) //gli utenti sono all'interno di una chiave nel db che sarebbe users
         .catch(error => console.error(error))
     },[]); //chiamata degli elementi in db solo al montaggio e quindi solo una volta
     
@@ -29,6 +35,7 @@ const [newUser, setNewUser] = useState({name:'', email:'', role:'user' });
         .catch(error => console.error(error))
     }
 
+    
 const deleteUser = (id) => {
     fetch(`http://localhost:5001/api/users/${id}`, {
             method:'DELETE'
@@ -42,6 +49,30 @@ const deleteUser = (id) => {
     return (
         <>
         <h1>Gestione degli utenti</h1>
+        <form onSubmit={postUser}>
+            <input type="text" placeholder="Name" value={newUser.name} onChange={(e) => setNewUser({...newUser, name:e.target.value})} required/>
+            <input type="email" placeholder="Email" value={newUser.email} onChange={(e) => setNewUser({...newUser, email:e.target.value})} required/>
+            <input type="date" placeholder="Date of Birth" value={newUser.date} onChange={(e) => setNewUser({...newUser, date:e.target.value})} />
+            <select value={newUser.role} onChange={(e) => setNewUser({...newUser, role:e.target.value})} required>
+                <option value="user">User</option>
+                <option value="Admin">Admin</option>
+            </select>
+            <button type="submit">Crea</button>
+        </form>
+
+        <button>
+            Back
+        </button>
+        <span>
+            {currentPage}/{totalPages}
+        </span>
+        <button>
+            Next
+        </button>
+        <select name="" id="" value={limit} onChange={(e) => setLimit(Number(e.target.value))}/>
+        <option value={5}></option>
+        <option value={10}></option>
+        <option value={20}></option>
         <ul>
             {
                 user.map((singleUsers) => (
@@ -55,16 +86,7 @@ const deleteUser = (id) => {
 
             }
         </ul>
-        <form onSubmit={postUser}>
-            <input type="text" placeholder="Name" value={newUser.name} onChange={(e) => setNewUser({...newUser, name:e.target.value})} required/>
-            <input type="email" placeholder="Email" value={newUser.email} onChange={(e) => setNewUser({...newUser, email:e.target.value})} required/>
-            <input type="date" placeholder="Date of Birth" value={newUser.date} onChange={(e) => setNewUser({...newUser, date:e.target.value})} />
-            <select value={newUser.role} onChange={(e) => setNewUser({...newUser, role:e.target.value})} required>
-                <option value="user">User</option>
-                <option value="Admin">Admin</option>
-            </select>
-            <button type="submit">Crea</button>
-        </form>
+
         </>
     )
 }
